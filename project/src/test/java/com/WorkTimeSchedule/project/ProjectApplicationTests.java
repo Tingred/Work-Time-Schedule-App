1bb56807600b0c871cb9598e547806e57ee3a920
Patryk-M <exiles1@wp.pl>
2021-08-18 12:33:02 +0200

    test

---
+++ project/src/test/java/com/WorkTimeSchedule/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WorkTimeSchedule.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
