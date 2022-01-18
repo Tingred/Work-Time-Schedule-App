1236a0994349f74fe447dbd293c660409dee7d42
Patryk-M <exiles1@wp.pl>
2022-01-18 21:47:22 +0100

    y

--- project/src/main/java/com/WorkTimeSchedule/project/module/security/security/WebSecurityConfig.java
+++ project/src/main/java/com/WorkTimeSchedule/project/module/security/security/WebSecurityConfig.java
@@ -3,6 +3,8 @@ import com.WorkTimeSchedule.project.module.security.security.jwt.AuthEntryPointJ
 import com.WorkTimeSchedule.project.module.security.security.jwt.AuthTokenFilter;
 import com.WorkTimeSchedule.project.module.security.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
